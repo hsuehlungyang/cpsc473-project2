6038b791ee0f702c1fc7cca2c2062a10fcbed2ea
tnguyen298 <tnguyen29@csu.fullerton.edu>
2017-11-18 01:15:00 -0800

    Uploaded from Github Desktop.
    Thao's first commit. Ember project set up with bootstrap sass and
    firebase. Some UI and css to the index page.

---
+++ config/environment.js
@@ -0,0 +1,66 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = function(environment) {
+  let ENV = {
+    modulePrefix: 'boba-meets-churros',
+    environment,
+    rootURL: '/',
+    locationType: 'auto',
+    firebase: {
+      apiKey: 'AIzaSyAqLN1EK57Hkw53IkJzFiCXm78dngk04QU',
+      authDomain: 'bobameetschrurros.firebaseapp.com',
+      databaseURL: 'https://bobameetschrurros.firebaseio.com',
+      projectId: 'bobameetschrurro',
+      storageBucket: '',
+      messagingSenderId: '842343871472'
+    },
+    // if using ember-cli-content-security-policy
+    contentSecurityPolicy: {
+      'script-src': "'self' 'unsafe-eval' apis.google.com",
+      'frame-src': "'self' https://*.firebaseapp.com",
+      'connect-src': "'self' wss://*.firebaseio.com https://*.googleapis.com"
+    },
+
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
+      }
+    },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    }
+  };
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+    // here you can enable a production-specific feature
+  }
+
+  return ENV;
+};
