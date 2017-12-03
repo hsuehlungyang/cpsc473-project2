fd1cb4ff79fe8cd01ebfe10698201d41be9dafbc
Alan Yang <hsuehlunguyang@csu.fullerton.edu>
2017-12-02 22:25:52 -0800

    database works now, info are stored to database. *NOTE: PLEASE DO NOT USE YOUR REAL PASSWORD, WE WILL SEE IT*

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,19 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-});
+
+    isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+    isDisabled: Ember.computed.not('isValid'),
+
+    actions: {
+
+      saveUser() {
+        const email = this.get('emailAddress');
+        const username = this.get('name');
+        const password = this.get('password');
+
+        const newInvitation = this.store.createRecord('invitation', { email: email, username: username, password: password });
+        newInvitation.save();
+      }
+    }
+  });
