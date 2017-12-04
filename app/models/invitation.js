0a59c46dca1d5c4fdbc27db53f3532b1849af6bc
tnguyen298 <tnguyen29@csu.fullerton.edu>
2017-12-03 17:22:17 -0800

    Add files via upload

--- app/models/invitation.js
+++ app/models/invitation.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  email: DS.attr('string')
+  email: DS.attr('string'),
+  username: DS.attr('string'),
+  password: DS.attr('string'),
+  gender: DS.attr('string'),
+  personalitytype: DS.attr('string'),
 });
