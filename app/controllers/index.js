983184854a5a195fb75f1ffb61040315f0b4641d
tnguyen298 <tnguyen29@csu.fullerton.edu>
2017-12-03 19:40:55 -0800

    Update index.js

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,13 +5,31 @@ export default Controller.extend({
     isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
     isDisabled: Ember.computed.not('isValid'),
 
+    init() {
+    this._super(...arguments);
+    this.items = [
+      { id: 1, name: 'Female' },
+      { id: 2, name: 'Male' },
+      { id: 3, name: 'Rather Not Say' }
+    ];
+    this.selectedItems = [];
+  },
+
     actions: {
+      valueHasChanged(item, isChecked) {
+        if (isChecked) {
+          this.get('selectedItems').addObject(item.name);
+        }
+        else {
+          this.get('selectedItems').removeObject(item.name);
+        }
+      },
 
       saveUser() {
         const email = this.get('emailAddress');
         const username = this.get('name');
         const password = this.get('password');
-        const gender = this.get('gender');
+        const gender = this.get('selectedItems');
         const personalitytype = this.get('personalitytype');
 
         const newInvitation = this.store.createRecord('invitation', { email: email, username: username, password: password, gender:gender, personalitytype:personalitytype });
