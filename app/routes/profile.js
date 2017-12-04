a160b92a2e1695f56cc3da2589a094b76a48c29e
Hieu Nguyen <hieunguyen626@gmail.com>
2017-12-04 11:09:26 -0800

    updated gravatar api [getting a binding issue, should work after that fix]

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,7 +1,5 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model: function(){
-    return this.store.findAll('invitation')
-  }
+
 });
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,6 +8,8 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('bower_components/JavaScript-MD5/js/md5.js');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
