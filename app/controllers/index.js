f93a190015a4c413573640ccac9b8430436a1638
tnguyen298 <tnguyen29@csu.fullerton.edu>
2017-12-03 20:38:56 -0800

    Update index.js

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -6,22 +6,78 @@ export default Controller.extend({
     isDisabled: Ember.computed.not('isValid'),
 
     init() {
-    this._super(...arguments);
-    this.items = [
+      this.genders = [
       { id: 1, name: 'Female' },
       { id: 2, name: 'Male' },
       { id: 3, name: 'Rather Not Say' }
     ];
-    this.selectedItems = [];
+    this.selectedGenders = [];
+
+    this.personalitytypes = [
+      { id: 1, name: 'Extrovert' },
+      { id: 2, name: 'Introvert' },
+      { id: 3, name: 'Ambivert' }
+    ];
+    this.selectedPersonalityTypes = [];
+
+    this.personalitytraits = [
+      { id: 1, name: 'Adventurous' },
+      { id: 2, name: 'Ambitious' },
+      { id: 3, name: 'Bubbly' },
+      { id: 4, name: 'Charistmatic' },
+      { id: 5, name: 'Clever' },
+      { id: 6, name: 'Compassionate' },
+      { id: 7, name: 'Confident' },
+      { id: 8, name: 'Considerate' },
+      { id: 10, name: 'Detail-Oriented' },
+      { id: 11, name: 'Easy-Going' },
+      { id: 12, name: 'Energetic' },
+      { id: 13, name: 'Generous' },
+      { id: 14, name: 'Humble' },
+      { id: 15, name: 'Impatient' },
+      { id: 16, name: 'Independent' },
+      { id: 17, name: 'Loyal' },
+      { id: 18, name: 'Observant' },
+      { id: 19, name: 'Open-Minded' },
+      { id: 20, name: 'Optimistic' },
+      { id: 21, name: 'Organized' },
+      { id: 22, name: 'Passionate' },
+      { id: 23, name: 'Patient' },
+      { id: 24, name: 'Perfectionist' },
+      { id: 25, name: 'Resourceful' },
+      { id: 26, name: 'Romantic' },
+      { id: 27, name: 'Suave' },
+      { id: 28, name: 'Sweet' },
+      { id: 29, name: 'Witty' }
+    ];
+    this.selectedPersonalityTraits = [];
   },
 
     actions: {
-      valueHasChanged(item, isChecked) {
+      valueHasChangedGender(gender, isChecked) {
         if (isChecked) {
-          this.get('selectedItems').addObject(item.name);
+          this.get('selectedGenders').addObject(gender.name);
         }
         else {
-          this.get('selectedItems').removeObject(item.name);
+          this.get('selectedGenders').removeObject(gender.name);
+        }
+      },
+
+      valueHasChangedPersonType(personalitytype, isChecked) {
+        if (isChecked) {
+          this.get('selectedPersonalityTypes').addObject(personalitytype.name);
+        }
+        else {
+          this.get('selectedPersonalityTypes').removeObject(personalitytype.name);
+        }
+      },
+
+      valueHasChangedPersonTrait(personalitytrait, isChecked) {
+        if (isChecked) {
+          this.get('selectedPersonalityTraits').addObject(personalitytrait.name);
+        }
+        else {
+          this.get('selectedPersonalityTraits').removeObject(personalitytrait.name);
         }
       },
 
@@ -29,10 +85,11 @@ export default Controller.extend({
         const email = this.get('emailAddress');
         const username = this.get('name');
         const password = this.get('password');
-        const gender = this.get('selectedItems');
-        const personalitytype = this.get('personalitytype');
+        const gender = this.get('selectedGenders');
+        const personalitytype = this.get('selectedPersonalityTypes');
+        const personalitytrait = this.get('selectedPersonalityTraits');
 
-        const newInvitation = this.store.createRecord('invitation', { email: email, username: username, password: password, gender:gender, personalitytype:personalitytype });
+        const newInvitation = this.store.createRecord('invitation', { email: email, username: username, password: password, gender:gender, personalitytype:personalitytype, personalitytrait:personalitytrait });
         newInvitation.save();
       }
     }
