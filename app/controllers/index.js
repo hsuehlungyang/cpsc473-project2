b0758f090bc129e930910743e41dd6e9e7b6d0e6
HieumongousNguyen <31426975+HieumongousNguyen@users.noreply.github.com>
2017-12-04 19:32:17 -0800

    .

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -100,25 +100,23 @@ export default Controller.extend({
         //alert(`Adding new user to our system: ${this.get('name')}`);
         const newInvitation = this.store.createRecord('invitation', { email: email, username: username, password: password, gender:gender, personalitytype:personalitytype, personalitytrait:personalitytrait });
         this.get('store').query('invitation', {filter: {name: 'thao'}}).then(function(thaos) {
-          //console.log(thaos);
           let usernameTaken = false;
           let i = 0;
           for(i = 0; i < thaos.content.length; ++i) {
-            //console.log(thaos.content[i]._data.username);
             if(username == thaos.content[i]._data.username) {
               alert("Username taken");
               usernameTaken = true;
-              this.transitionToRoute('index');
               break;
             }
           }
           if(!usernameTaken) {
-            alert("Hi " + username);
+            //alert("Hi " + username);
           }
           else{
-            this.transitionToRoute('index');
+            this.refresh();
           }
         newInvitation.save();
+
         this.set('responseMessage', `Thank you! We've just saved your profile.`);
         this.set('emailAddress', '');
         this.set('name', '');
--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,9 +1,13 @@
 import Controller from '@ember/controller';
 
-
 export default Controller.extend({
+  gender: '',
   name: '',
   password: '',
+  username: '',
+  type: '',
+  trait: '',
+
 
   isEmptyName: Ember.computed.empty('name'),
   isValidName: Ember.computed.not('isEmptyName'),
@@ -13,11 +17,39 @@ export default Controller.extend({
   isDisabled: Ember.computed.not('isValid'),
 
   actions: {
-   sendMessage() {
-     alert(`Sending the following message: ${this.get('message')}`);
-     this.set('responseMessage', `Thank you! Your message has been sent! Please wait for our response to ${this.get('emailAddress')}.`);
-     this.set('emailAddress', '');
-     this.set('message', '');
+    login() {
+    this.set('name', '');
+    this.set('password', '');
+    this.set('gender', '');
+  },
+
+   authenticateUser() {
+
+     const username = this.get('name');
+     const password = this.get('password');
+     let gender = '';
+
+     this.get('store').query('invitation', {filter: {name: ''}}).then(function(thaos) {
+       let usernameAuthenticate = false;
+       let i = 0;
+       for(i = 0; i < thaos.content.length; ++i) {
+         if(username == thaos.content[i]._data.username && password == thaos.content[i]._data.password) {
+           document.getElementById("thisGender").innerHTML = thaos.content[i]._data.gender;
+           document.getElementById("thisUser").innerHTML = username;
+           document.getElementById("thisPersonalityType").innerHTML = thaos.content[i]._data.personalitytype;
+           document.getElementById("thisPersonalityTrait").innerHTML = thaos.content[i]._data.personalitytrait;
+           usernameAuthenticate = true;
+           break;
+         }
+       }
+       if(usernameAuthenticate) {
+         alert("Hi " + username);
+       }
+       else{
+         alert("Wrong username or password");
+         this.refresh();
+       }
+     });
    }
  }
 });
